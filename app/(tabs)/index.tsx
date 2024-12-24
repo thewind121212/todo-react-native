bf6c605006381b0bd7950695ac398296fa013928
Linh Tran <52797453+thewind121212@users.noreply.github.com>
2024-12-25 00:11:41 +0700

    add animation tick on habit and init task overview

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
-import { View, Text, StyleSheet, ScrollView, RefreshControl } from 'react-native'
-import React, {  useState } from 'react'
+import { View, Text, StyleSheet, ScrollView, RefreshControl, Dimensions } from 'react-native'
+import React, { useState } from 'react'
 import BlockHeader from '@/components/BlockHeader'
 
 import OverviewHomeTask from '@/components/OverviewHomeTask'
 import { useOnboardingPersisStore } from '@/store/useOnboarding'
 import OverviewHomeHabit from '@/components/OverviewHomeHabit'
 import HabitItem from '@/components/HabitItem'
+import CircularProgress from '@/components/CircleProgress'
 
 
 
@@ -43,7 +44,7 @@ const Index = () => {
     }
   };
 
-
+  const { width } = Dimensions.get('window');
 
 
 
@@ -70,13 +71,29 @@ const Index = () => {
       <BlockHeader isShowSubTitle={true} mainTitle="Task Overview" subTitle="see all" isShowBoxCount={true} boxCount={8} />
       <OverviewHomeTask />
       <BlockHeader isShowSubTitle={false} mainTitle="In Progress Habit" subTitle="see all" isShowBoxCount={true} boxCount={2} />
-      <View style={{ flexDirection: 'column', width: '100%', height: 300, overflow: 'hidden', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 14 }}>
+      <View style={{ flexDirection: 'column', width: '100%', height: 300, overflow: 'hidden', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 14, marginBottom: 20 }}>
 
         <HabitItem cardContent="Drink water" primaryColor="#FF748B" />
         <HabitItem cardContent="Go To The Gym" primaryColor="#3068DF" />
         <HabitItem cardContent="Eat More Clean" primaryColor="#6861ED" />
         <HabitItem cardContent="Doing Some Coding" primaryColor="#FF748B" />
         <HabitItem cardContent="Sleep Well Is Best Medicine" primaryColor="#FF748B" />
+      </View>
+
+      <BlockHeader isShowSubTitle={true} mainTitle="In Progress Task" subTitle="see all" isShowBoxCount={true} boxCount={2} />
+
+      <View style={{ flexDirection: 'column', width: '100%', height: 300, overflow: 'hidden', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 14, marginBottom: 20 }}>
+
+        <View style={{ width: '100%', paddingVertical: 32, backgroundColor: '#222239', borderRadius: 8, paddingRight: 20, paddingLeft: 24, display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row', position: "relative", overflow: 'hidden' }}>
+          <Text style={{ color: "#FFFFFF", fontSize: 18, fontWeight: "400" }}>Learning 3D modal in cinema 4D</Text>
+          <View style={[{ position: 'absolute', right: 20 }]} >
+            <View style={{ position: "relative", display: "flex", justifyContent: "center", alignItems: "center" }}>
+              <CircularProgress progress={40} strokeWidth={8} strokeWidthFull={4} size={60} rotate='-90' color='#6861ED' animationDirection='clockwise' />
+              <Text style={{ position: "absolute", color: "white", fontWeight: 600, fontSize: 14 }}>40%</Text>
+            </View>
+          </View>
+          <View style={{ width: (width - 40), height: 3, position: "absolute", left: 0, bottom: 0, borderRadius: 4, overflow: 'hidden' ,backgroundColor: "#6861ED" }}></View>
+        </View>
       </View>
 
     </ScrollView>
--- components/HabitItem.tsx
+++ components/HabitItem.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import { View, Text, Dimensions } from 'react-native'
-import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming } from "react-native-reanimated";
+import { View, Text, Dimensions, Pressable } from 'react-native'
+import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withSpring, withTiming } from "react-native-reanimated";
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 
@@ -13,14 +13,27 @@ const HabitItem = ({ cardContent, primaryColor }: Props) => {
 
     const [isDone, setIsDone] = useState(false)
 
+    const scale = useSharedValue(0);
 
-    const { width } = Dimensions.get('window')
+    const animatedStyleScale = useAnimatedStyle(() => {
+        return {
+            transform: [{ scale: scale.value }],
+        };
+    });
+
+
+
+
     const opacity = useSharedValue(1);
     const animatedStyle = useAnimatedStyle(() => {
         return {
             opacity: opacity.value,
         };
     });
+
+    useEffect(() => {
+        scale.value = withTiming(isDone ? 1 : 0, {duration: 200});
+    }, [scale, isDone]);
 
 
     useEffect(() => {
@@ -32,10 +45,18 @@ const HabitItem = ({ cardContent, primaryColor }: Props) => {
     }, [opacity]);
     return (
         <View style={{ width: '100%', paddingVertical: 20, backgroundColor: '#222239', borderRadius: 8, paddingRight: 20, paddingLeft: 34, display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row', position: "relative" }}>
-            <Animated.View style={[{ width: 4, height: 38, backgroundColor: primaryColor, position: "absolute", left: 20, borderTopLeftRadius: 8, borderTopRightRadius: 8, borderBottomRightRadius: 8, borderBottomLeftRadius: 8 }, animatedStyle]} />
-            <Text style={{ color: "#FFFFFF", fontSize: 18, fontWeight: "400" }}>{cardContent}</Text>
-            <View style={{ width: 24, height: 24, borderRadius: "50%", borderColor: primaryColor, borderWidth: 2.4 }}></View>
-            <FontAwesome name="check-circle" size={28} color="#737379" style={{ position: 'absolute', right: 20 }} />
+            <Animated.View style={[{ width: 4, height: 38, backgroundColor: isDone? "#737379" : primaryColor, position: "absolute", left: 20, borderTopLeftRadius: 8, borderTopRightRadius: 8, borderBottomRightRadius: 8, borderBottomLeftRadius: 8 }, animatedStyle]} />
+            <Text style={{ color: isDone ? "#737379" : "#FFFFFF", fontSize: 18, fontWeight: "400", textDecorationLine: isDone ? 'line-through' : "none"  }}>{cardContent}</Text>
+            <Pressable style={{ width: 24, height: 24, borderRadius: "50%", borderColor: primaryColor, borderWidth: 2.4 , opacity: !isDone ? 1 : 0 }}
+                onTouchStart={() => setIsDone(true)}
+            ></Pressable>
+            <Animated.View style={[{ position: 'absolute', right: 20 }, animatedStyleScale]} >
+                <Pressable
+                    onPress={() => setIsDone(false)}
+                >
+                    <FontAwesome name="check-circle" size={28} color="#737379" />
+                </Pressable>
+            </Animated.View>
         </View>
     )
 }
